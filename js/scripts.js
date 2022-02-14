e54808296aba031d21fc71665471005ff8f62527
Filipe Magalhães Sena <filipemagalhaessena@gmail.com>
2022-02-14 10:27:27 -0300

    1

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,83 @@
-class Validator{
-    // Constructor serve para inicar as propriedades que um objeto possui.
-    constructor(){
-        this.validations=[
+class Validator {
 
+    constructor(){
+        this.validations = [
+            'data-min-length',
         ]
     }
-    //Iniciar a validação de todos os campos
+
+    //iniciar a validação de todos os campos
 
     validate(form){
-        //pegar os inputs
-        let inputs = form.getElementByTagName('input');
+
+        let inputs = document.getElementsByTagName('input')
 
         console.log(inputs);
 
-        //HTMLCollection -> ARRAY
+        // Transformo uma HTMLCollection - > array
 
-        let inputsArray = [... inputs];
+        let inputsArray = [...inputs];
 
         console.log(inputsArray);
 
+        //Loop nos inputs e validação mediante ao que for encontrado
+
+        inputsArray.forEach(function(input){
+
+
+            //Loop com todas as validações existentes
+
+            for(let i = 0; this.validations.length > i; i++){
+                //Verifica se a validação atual existe no input
+                if(input.getAttribute(this.validations[i]) != null){
+
+                    let method=this.validations[i].replace('data-', '').replace('-','');
+
+                    let value = input.getAttribute(this.validations[i]);
+
+                    this[method](input, value);
+
+                }
+            }
+
+        }, this);
+
+    }
+
+    minlength(input, minValue){
+        let inputLenght= input.value.length;
+        let errorMessage= `O campo precisa ter pelo menos ${minValue} caracteres`;
+        
+        if(inputLenght < minValue){
+            this.printMessage(input, errorMessage);
+        }
+    }
+    //Método para imprimir mensagem de erro na tela
+    printMessage(input, msg){
+        let template = document.querySelector('.error-validation').cloneNode(true);
+        
+        template.textContent = msg;
+
+        let inputParent = input.parentNode;
+
+        template.classList.remove('template');
+
+        inputParent.appendChild(template)
     }
 }
 
-
 let form = document.getElementById("register-form");
-let submit = document.getElementById("btn-submit")
+let submit = document.getElementById("btn-submit");
 
-let validator= new Validator();
+let validator = new Validator();
 
-// Evento que dispara as validações
+//Evento que dispara as validações
 
-submit.addEventListener("click", function(e){
-    
-    e.preventDefault()
-    
+submit.addEventListener('click', function(e){
+
+    e.preventDefault();
 
     validator.validate(form);
 
-    })
+})
+
